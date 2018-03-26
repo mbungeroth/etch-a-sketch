ff2fdfe0f6cabb2cd39d99038a92457b8dc6f4ac
Mark Bungeroth <mbungeroth@gmail.com>
2018-03-26 18:19:57 -0400

    Add grid creation javascript

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,5 +1,16 @@
 function createGrid(numBoxes=16) {
-  let columns = `grid-temple-columns: repeat(${numBoxes}, 1fr)`;
-  let rows = `grid-template-rows: repeat(${numBoxes}, 1fr)`;
+  let columns = `repeat(${numBoxes}, 1fr)`;
+  let rows = `repeat(${numBoxes}, 1fr)`;
+  const container = document.querySelector('.container');
+  container.style.gridTemplateColumns = columns;
+  container.style.gridTemplateRows = rows;
 
+  for (let i = 0; i < numBoxes * numBoxes; i++) {
+    let box = document.createElement('div');
+    box.setAttribute('class', i+1);
+    container.appendChild(box);
+    box.style.border = '1px solid black';
+  }
 }
+
+createGrid();
