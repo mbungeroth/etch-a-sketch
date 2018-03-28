e4834109ec16191f73950e298de0f731bfa8cf58
Mark Bungeroth <mbungeroth@gmail.com>
2018-03-27 22:41:32 -0400

    Use foreach to make clear function work. add reset functionality. make darker upon hover.

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,16 +1,59 @@
-function createGrid(numBoxes=16) {
+function createGrid(event, numBoxes=16) {
   let columns = `repeat(${numBoxes}, 1fr)`;
   let rows = `repeat(${numBoxes}, 1fr)`;
-  const container = document.querySelector('.container');
   container.style.gridTemplateColumns = columns;
   container.style.gridTemplateRows = rows;
+  let totalSquares = numBoxes * numBoxes;
 
-  for (let i = 0; i < numBoxes * numBoxes; i++) {
+  for (let i = 0; i < totalSquares; i++) {
     let box = document.createElement('div');
-    box.setAttribute('class', i+1);
+    box.setAttribute('class', 'box');
     container.appendChild(box);
     box.style.border = '1px solid black';
+    box.addEventListener('mouseover', makeDarker);
   }
 }
+
+
+function resetGrid() {
+  clearGrid();
+  createGrid(event);
+}
+
+
+function chooseGrid() {
+  let num = prompt('Please enter a number between 2 and 100');
+
+  if (Number(num) >= 2 && Number(num) <= 100) {
+    clearGrid();
+    createGrid(event, Number(num));
+  } else {
+    alert('Follow directions, yo. Try again.');
+  }
+}
+
+
+function makeDarker() {
+  let currentOp = this.style.opacity;
+  let newOp = Number(currentOp) + 0.1;
+  this.style.opacity = newOp;
+}
+
+
+function clearGrid() {
+  document.querySelectorAll('.box').forEach(function(div) {
+    container.removeChild(div);
+  });
+}
+
+const whatGrid = document.getElementById('choose');
+whatGrid.addEventListener('click', chooseGrid);
+
+const reset = document.getElementById('reset');
+reset.addEventListener('click', resetGrid);
+
+const container = document.querySelector('.container');
+
+
 
 createGrid();
